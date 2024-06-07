370e03211cbd18fd7aa5368170f63e1cc6e5b5fe
Jemeric <tyuity14450@hotmail.com>
2024-06-06 20:46:58 -0400

    Modified index, removed index.html, slightly modified routes. and commented in a few ideas

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -4,4 +4,4 @@
 // has the username, and number record of played adventures, maybe a score system. 
 
 // table 3: Explorer
-// has name based of username(foreign key), the scenario the explorer is currently in, Booleans based on if they have items or not.
+// has name based of username(foreign key), the scenario the explorer is currently in(foreign key), Booleans based on if they have items or not.
