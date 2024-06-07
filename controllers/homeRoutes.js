fb60f46843edc0754988b308881eaa34716d3741
Jemeric <tyuity14450@hotmail.com>
2024-06-06 23:02:10 -0400

    Added values to game,login,trgister, ande scenario handlebars. added Get request to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async(req,res)=> {
-    res.render('./layouts/main');
+    res.render('./partials/game');
+});
+
+router.get('/login', async(req,res)=> {
+    return res.render('./partials/login');
+});
+
+router.get('/register', async(req,res) =>{
+    return res.render('./partials/register');
+});
+
+router.get('/bypass',async (req,res) => {
+     res.render('./partials/game', {
+    loggedIn: true
+    });
 });
 
 
