fee7f121871be1facebc68fb62741cc75331524a
Jemeric <tyuity14450@hotmail.com>
2024-06-10 00:21:15 -0400

    Updated look of scenario

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Configure session and cookies
-// const sess = {
-//   secret: process.env.SESSION_SECRET,
-//   cookie: {
-//     maxAge: 300000,
-//     httpOnly: true,
-//     secure: false,
-//     sameSite: 'strict',
-//   },
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
+Configure session and cookies
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 // Set up Handlebars.js as the template engine
 app.engine('handlebars', exphbs.engine());
