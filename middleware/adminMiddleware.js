4739903bd19393e122f62f3e2606a6dad2f1e430
cnm724 <cnm724@gmail.com>
2024-06-17 14:23:56 -0600

    feat: accessDenied Page

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -2,7 +2,7 @@ const ensureAdmin = (req, res, next) => {
     if (req.session && req.session.username && req.session.role === 'admin') {
       next();
     } else {
-      res.status(403).json({ message: 'Access denied, admin only.' });
+      res.render('./partials/accessDenied');
     }
   };
   
